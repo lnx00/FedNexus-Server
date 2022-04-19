ec22025b49c45a80807257f232ba8cddaf0cf231
lnx00 <linch255@gmail.com>
2022-04-19 20:21:11 +0200

    Added verbose logging

--- settings.js
+++ settings.js
@@ -3,3 +3,4 @@ const options = require('command-line-args')({ name: "port", type: Number });
 exports.LISTEN = options.port || 3000;
 exports.enforce_limits = true;
 exports.enable_endpoint_proxy = true;
+exports.Verbose = true;
--- v1.js
+++ v1.js
@@ -1,9 +1,10 @@
 var lib = require("./lib.js");
+const settings = require("./settings.js");
 
-module.exports = handleConnect;
+module.exports = handleConnection;
 
-function handlePlayerTF2ServerConnect(client) {
-    // User is connected to a server
+// Handle the player connecting to a TF2 server
+function handlePlayerServer(client) {
     if (client.server) {
         var clients = lib.getClientsOnServer(client.server.ip, client.server.port);
         var steamidlist = [];
@@ -19,15 +20,20 @@ function handlePlayerTF2ServerConnect(client) {
 
 const isPrintableASCII = string => /^[\x20-\x7F]*$/.test(string);
 
-function onmsg(client, msg) {
+// Handle incoming messages from the client
+function onMessage(client, msg) {
     try {
         var obj = JSON.parse(msg);
         // Check if this message contains everything we need
-        if (typeof obj.username != "string" || typeof obj.type != "string" || typeof obj.data != "object")
+        if (typeof obj.username != "string" || typeof obj.type != "string" || typeof obj.data != "object") {
             return;
+        }
+
         // Make sure the username isn't too large
-        if (obj.username.length > 32)
+        if (obj.username.length > 32) {
             return;
+        }
+
         if (!isPrintableASCII(obj.username))
             return;
 
@@ -37,38 +43,63 @@ function onmsg(client, msg) {
                 // loc: Which chat? Currently only "public" is supported
                 // msg: Actual message
 
-                if (typeof obj.data.msg != "string" || typeof obj.data.loc != "string")
+                if (typeof obj.data.msg != "string" || typeof obj.data.loc != "string") {
                     return;
+                }
 
                 // Check if the message is too big/wrong location
-                if (obj.data.msg.length > 128 || obj.data.loc != "public")
+                if (obj.data.msg.length > 128 || obj.data.loc != "public") {
                     return;
+                }
 
                 // Check if message is printable ascii
-                if (!isPrintableASCII(obj.data.msg))
+                if (!isPrintableASCII(obj.data.msg)) {
                     return;
+                }
+
+                if (settings.Verbose) {
+                    console.log("[CHAT] " + obj.username + ": " + obj.data.msg);
+                }
+
                 lib.handleChatMessage(client.ip, obj.username, obj.data.msg, "public", client.colour)
                 break;
+
             case "dataupdate":
-                if (typeof obj.data.colour == "string")
+                if (typeof obj.data.colour == "string") {
+                    if (settings.Verbose) {
+                        console.log("[DataUpdate] Color will be changed to: " + obj.data.colour);
+                    }
+
                     lib.setColour(client, obj.data.colour);
+                }
+
                 if (typeof obj.data.server == "object") {
                     if (obj.data.server.connected) {
+                        if (settings.Verbose) {
+                            console.log("[DataUpdate] Server will be changed to: " + obj.data.server.ip + ":" + obj.data.server.port);
+                        }
+
                         lib.setServer(client, obj.data.server.ip, obj.data.server.port, obj.data.server.steamid);
-                        handlePlayerTF2ServerConnect(client);
+                        handlePlayerServer(client);
                     }
-                    else
+                    else {
+                        if (settings.Verbose) {
+                            console.log("[DataUpdate] Server will be changed to: null");
+                        }
+
                         lib.setServer(client);
+                    }
                 }
                 break;
+
             default:
                 return;
         }
 
-    } catch (error) { console.log(error); return; }
+    } catch (error) { console.error(error); return; }
 }
 
-function handleConnect(client) {
-    client.onmsg = onmsg;
-    handlePlayerTF2ServerConnect(client);
+function handleConnection(client) {
+    client.onmsg = onMessage;
+    handlePlayerServer(client);
 }
